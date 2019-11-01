9c2504c05011a15a1e192d8cb5048edea63aed88
florim <florimsejdiu@gmail.com>
2019-11-02 00:18:17 +0100

    Maybe final

--- app/src/main/java/com/example/grupi6/MainActivity.java
+++ app/src/main/java/com/example/grupi6/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,8 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import com.example.grupi6.Colors;
+
+import static android.os.Build.VERSION_CODES.P;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("metoda e thirrur eshte onCreate");
 
         editText = findViewById(R.id.editText);
         button1 = findViewById(R.id.button);
@@ -79,8 +83,10 @@ public class MainActivity extends AppCompatActivity {
     }
     private void showAlertDialog() {
 
+        final Toast toast = Toast.makeText(this, "Color was changed succesfuly", Toast.LENGTH_SHORT);
 
-       AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setTitle("Change color");
         alertDialogBuilder.setMessage("Do you want to change background color?");
         alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -88,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 relativeLayout.setBackgroundColor(color.getColor());
 
-
+                toast.show();
               //  finish();
             }
         });
@@ -98,7 +104,53 @@ public class MainActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
+
+
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("metoda e thirrur eshte onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("metoda e thirrur eshte onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("metoda e thirrur eshte onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("metoda e thirrur eshte onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("metoda e thirrur eshte onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("metoda e thirrur eshte onDestroy");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        System.out.println("metoda e thirrur eshte onBackPressed");
+    }
+
+
 }
--- app/src/main/java/com/example/grupi6/SecondActivity.java
+++ app/src/main/java/com/example/grupi6/SecondActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
     TextView textView;
+
+    Button button3;
+    Colors color = new Colors();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,7 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         textView = findViewById(R.id.textView2);
+        button3 = findViewById(R.id.button3);
 
         if (getIntent().getExtras() != null){
             String vlera = getIntent().getStringExtra("celesi");
@@ -23,6 +29,14 @@ public class SecondActivity extends AppCompatActivity {
         }else {
             Toast.makeText(this, "Extras are null", Toast.LENGTH_SHORT).show();
         }
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setTextColor(color.getColor());
+            }
+        });
+
     }
 
     @Override
